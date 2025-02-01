19f75eaf1db2f50fbb3c011939ec25372dc9bb4e
giaosoissomsm <114545006+giaosoissomsm@users.noreply.github.com>
2025-02-01 18:18:00 -0300

    Update goose.ps1

--- scripts/goose.ps1
+++ scripts/goose.ps1
@@ -13,16 +13,5 @@ Expand-Archive -Path $zipPath -DestinationPath $tempPath -Force
 Move-Item -Path "$tempPath\DesktopGoose v0.31\*" -Destination $tempPath -Force
 Remove-Item -Path "$tempPath\DesktopGoose v0.31" -Recurse -Force
 
-# Criar ação para rodar o Desktop Goose
-$action = New-ScheduledTaskAction -Execute $tempPath
-
-# Gatilho para iniciar APENAS no login do usuário
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-
-# Configurar a tarefa para rodar com privilégios normais do usuário logado
-$principal = New-ScheduledTaskPrincipal -UserId "$env:UserName" -LogonType Interactive -RunLevel Limited
-
-# Criar e registrar a tarefa agendada
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
-Register-ScheduledTask -TaskName $taskName -InputObject $task -Force
+Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute "powershell.exe" -Argument "Start-Process -FilePath '$env:TEMP\goose\GooseDesktop.exe'") -Trigger (New-ScheduledTaskTrigger -AtLogon) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries) -TaskName "GooseDesktopAtLogon" -Description "Executa o GooseDesktop.exe sempre que o usuário logar"
 Start-Process -FilePath "$env:TEMP\goose\GooseDesktop.exe"
